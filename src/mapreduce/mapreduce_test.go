e1b89d2b233b2837e1da4298d632d0b7cc5f8898
liuwenyi <liuwenyi@baidu.com>
2017-03-12 10:43:32 +0800

    add work's register and rpc code

--- src/mapreduce/mapreduce_test.go
+++ src/mapreduce/mapreduce_test.go
@@ -12,9 +12,9 @@ func Test(t *testing.T) {
 	var wk1, wk2, wk3 Worker
 	//init the mapreduce struct
 	m.Init()
-	wk1.Init()
-	wk2.Init()
-	wk3.Init()
+	wk1.Init("127.0.0.1", 20001)
+	wk2.Init("127.0.0.1", 20002)
+	wk3.Init("127.0.0.1", 20003)
 	//start mapreduce
 	m.Start()
 	wk1.Start()
--- src/mapreduce/worker.go
+++ src/mapreduce/worker.go
@@ -1,11 +1,55 @@
 package mapreduce
 
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	_"google.golang.org/grpc/reflection"
+
+	pb "proto"
+)
+
 type Worker struct {
-	
+	masterIp	string
+	masterPort	int
+	ip string
+	port int
+}
+
+// register to master
+func (w *Worker)register(c pb.MasterClient) error {
+	registerRequest := &pb.RegisterRequest{
+		Ip			: w.ip,
+		Port		: int32(w.port),
+	}
+	r, err := c.Register(context.Background(), registerRequest)
+	if err != nil {
+		fmt.Printf("could not register: %v", err)
+		os.Exit(1)
+	}
+	fmt.Println(r)
+	return err
 }
 
 func (w *Worker)Start() {
+	//connect to master
+	conn, err := grpc.Dial(MASTER_ADDRESS, grpc.WithInsecure())
+	if err != nil {
+		fmt.Printf("did not connect: %v", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+	c := pb.NewMasterClient(conn)
+	err = w.register(c)
+	if err != nil {
+		fmt.Printf("blockServer register error %s\n", err)
+		os.Exit(1)
+	}
 }
 
-func (w *Worker)Init() {
+func (w *Worker)Init(ip string, port int) {
+	w.ip = ip
+	w.port = port
 }
