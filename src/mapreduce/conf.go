8b14b204763e2a394e27aa366adffa0f99a485aa
liuwenyi <liuwenyi@baidu.com>
2017-03-12 10:07:58 +0800

    add master's start rpc server

--- src/mapreduce/conf.go
+++ src/mapreduce/conf.go
@@ -3,4 +3,5 @@ package mapreduce
 const (
 	MASTER_IP 		= "127.0.0.1"
 	MASTER_PORT 	= 20000
+	MASTER_ADDRESS	= "127.0.0.1:20000"
 )
--- src/mapreduce/master.go
+++ src/mapreduce/master.go
@@ -2,30 +2,61 @@ package mapreduce
 
 import (
 	"fmt"
-	_"net"
+	"os"
+	"net"
 
-	_"golang.org/x/net/context"
-	_"google.golang.org/grpc"
-	_"google.golang.org/grpc/reflection"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 
-	_ "proto"
+	pb "proto"
 )
 
 type Master struct {
 	ip string
 	port int
+	nWorker int
+	workerDone chan bool
 }
+
+var m Master
 
 func (m *Master)Init() {
 	m.ip = MASTER_IP
 	m.port = MASTER_PORT
+	m.nWorker = 0
 	fmt.Println("Master Init OK!")
+}
+
+func (m *Master) Register(ctx context.Context, in *pb.RegisterRequest) (*pb.RegisterResponse, error) {
+	return &pb.RegisterResponse{}, nil
+}
+
+func (m *Master) Reduce(ctx context.Context, in *pb.ReduceRequest) (*pb.ReduceResponse, error) {
+	//wait for map report
+	//print reduce result
+	return &pb.ReduceResponse{}, nil
+}
+
+// startBlockServer start blockserver (rpc)
+func startMaster() {
+	lis, err := net.Listen("tcp", MASTER_ADDRESS)
+	if err != nil {
+		fmt.Printf("failed to listen: %v", err)
+		os.Exit(1)
+	}
+	s := grpc.NewServer()
+	pb.RegisterMasterServer(s, &m)
+	//Register reflection service on gRPC server.
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		fmt.Printf("failed to serve: %v", err)
+		os.Exit(1)
+	}
 }
 
 func (m *Master)Start() {
 	fmt.Println("Master Start..")
 	//start master rpc server
-	
-	//wait for map report
-	//print reduce result
+	go startMaster()
 }
